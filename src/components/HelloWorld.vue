a4148179d2545347e954efb950a43ad003383535
DESKTOP-G7VBR0R\WEIMOB <zhigang.zhang@weimob.com>
2022-04-12 17:55:10 +0800

    change

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,7 +14,7 @@ export default defineComponent({
     msg: String,
   },
   setup() {
-    let name = ref("zhangsan");
+    let name = ref("zhangsan222");
 
     const changeName = () => {
       name.value = "lisi";
