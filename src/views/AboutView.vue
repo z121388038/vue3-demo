143d2fc55873db0e10b49d10abcf86b4c30c0d46
DESKTOP-G7VBR0R\WEIMOB <zhigang.zhang@weimob.com>
2022-04-17 12:50:32 +0800

    change

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="about">
     <TestObjectProps :="person" six="女"></TestObjectProps>
+    <TestAttr
+      title="这是title"
+      class="sdf"
+      style="color: red"
+      :value="person.age"
+      @change="myClick"
+    ></TestAttr>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import TestObjectProps from "./components/TestObjectProps";
+import TestAttr from "./components/TestAttr";
 import Persion from "@/views/Persion";
 
 export default defineComponent({
   components: {
     TestObjectProps,
+    TestAttr,
   },
   data() {
     return {
@@ -20,6 +29,11 @@ export default defineComponent({
         age: 2533,
       },
     };
+  },
+  methods: {
+    myClick(e) {
+      console.log("fu click", e.target.value);
+    },
   },
 });
 </script>
---
+++ src/views/components/TestAttr.vue
@@ -0,0 +1,19 @@
+<template>
+  <input type="text" />
+  <!--  <div>-->
+  <!--    <p>组件的props: {{ name }}</p>-->
+  <!--    <p>不在props里面的attr: {{ $attrs }}</p>-->
+  <!--  </div>-->
+</template>
+
+<script>
+export default {
+  name: "TestAttr",
+  // inheritAttrs: false,
+  props: {
+    name: String,
+  },
+};
+</script>
+
+<style scoped></style>
