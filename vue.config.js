cec3ca7a43542ebcc9648cf6334df682fca1a8c9
DESKTOP-G7VBR0R\WEIMOB <zhigang.zhang@weimob.com>
2022-05-31 17:54:12 +0800

    add yml

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ const Components = require('unplugin-vue-components/webpack');
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers');
 
 module.exports = defineConfig({
+  publicPath: '/vue3-demo/',
   transpileDependencies: true,
   configureWebpack: {
     plugins: [
