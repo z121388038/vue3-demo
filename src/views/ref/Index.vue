9fc9631f3c837f776c86c5463550e620051e37d8
DESKTOP-G7VBR0R\WEIMOB <zhigang.zhang@weimob.com>
2022-08-04 22:31:30 +0800

    reactive

--- src/views/ref/Index.vue
+++ src/views/ref/Index.vue
@@ -1,15 +1,20 @@
 <template>
   <h3>ref</h3>
+  <div>{{ state }}</div>
+  <button @click="handelClick">按钮</button>
 </template>
 
 <script lang="ts" setup>
 import { reactive, ref, toRef, unref } from 'vue';
 
-// const name1 = '张三';
-// const name2 = ref('李四');
-//
-// console.log(unref(name1), unref(name2));
+/*
+const name1 = '张三';
+const name2 = ref('李四');
 
+console.log(unref(name1), unref(name2));
+*/
+
+/*
 const state = reactive({
   foo: 1,
   bar: 2,
@@ -23,6 +28,14 @@ state.foo++;
 console.log(state, fooRef.value);
 
 console.log(state.foo === fooRef.value);
+*/
+
+let obj = { name: 'lin' };
+let state = toRef(obj, 'name');
+const handelClick = () => {
+  state.value = 'sdf';
+  console.log(state.value, obj.name);
+};
 </script>
 
 <style scoped lang="scss"></style>
